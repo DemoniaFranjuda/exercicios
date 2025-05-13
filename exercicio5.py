7f0fea69deb627fa78ec511197b6d478c3c9d020
DemoniaFranjuda <00001106703194sp@al.educacao.sp.gov.br>
2025-05-13 08:12:54 -0300

    Cadastro de Alunos

--- exercicio5.py
+++ exercicio5.py
@@ -1,18 +1,35 @@
 from exercicio4 import calcular_media
 
+alunos = []
+
+def obter_dados_aluno():
+   nome_aluno = input("Informe o Nome Completo do Aluno: ")
+   email_aluno = input("Informe o Email do Aluno: ")
+   serie_aluno = input("Informe a Série do Aluno: ")
+   nota01_aluno = int(input("Informe a Primeira Nota do Aluno: "))
+   nota02_aluno = int(input("Informe a Segunda Nota do Aluno: "))
+   nota03_aluno = int(input("Informe a Terceira Nota do Aluno: "))
+
 def cadastrar_aluno(nome, email, serie, nota01=0, nota02=0, nota03=0):
-    alunos = []
+
 
     notas = [nota01, nota02, nota03]
-
+    
     aluno = {
         "nome": nome,
         "email": email,
         "serie": serie,
         "notas": notas,
         "media": calcular_media(notas)
+   
     }
 
     alunos.append(aluno)
 
+   
     return alunos
+
+def mostrar_dados_alunos(dados_alunos):
+    return print(dados_alunos)
+
+mostrar_dados_alunos(alunos)
